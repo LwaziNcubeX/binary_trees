5c714605ff9af77b7f65053e970a3f120e72c450
LwaziNcubeX <lwazincubex@gmail.com>
2023-10-31 16:49:11 +0200

    Updated header file for binary trees implementation

--- binary_trees.h
+++ binary_trees.h
@@ -14,10 +14,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 /*BASIC BINARY TREE*/
 typedef struct binary_tree_s binary_tree_t;
@@ -33,5 +33,6 @@ typedef struct binary_tree_s heap_t;
 /**FUNCTION PROTOTYPES**/
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 #endif /* _BINARY_TREES_H_ */

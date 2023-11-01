631a7b9394ce40d416654e0265c125e10e478973
LwaziNcubeX <lwazincubex@gmail.com>
2023-11-02 00:51:55 +0200

    A function that measures the balance factor of a binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,29 +1,58 @@
 #include "binary_trees.h"
 /**
- * binary_tree_balance - Measures the balance factor of a binary tree.
+ * max - Finds the maximum of two integers
  *
- * @tree: A pointer to the root node of the tree to measure the balance factor.
+ * @a: First integer
+ * @b: Second integer
  *
- * Return: 0 or The balance factor of the binary tree.
+ * Return: The maximum value between @a and @b
  */
-int binary_tree_balance(const binary_tree_t *tree)
+int max(int a, int b)
 {
-	int left_avail = tree && tree->left;
-	int right_avail = tree && tree->right;
-	int x = 0;
+	return ((a > b) ? a : b);
+}
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree:  a pointer to the root node of the tree
+ *
+ * Return: if tree is NULL, return 0 else return height
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int left, right, height;
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
 
-	if (left_avail && right_avail)
-	{
-		x = 2;
-	}
-	else if (left_avail || right_avail)
-	{
-		x = -1;
-	}
-	return (x);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	height = max(left, right) + 1;
+
+	return (height);
+}
+
+
+
+/**
+ * binary_tree_balance - Measures the balance factor of a binary tree.
+ *
+ * @tree: A pointer to the root node of the tree to measure the balance factor.
+ *
+ * Return: The balance factor of the binary tree.
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left_height, right_height, x, y;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	x = left_height - right_height >= 2;
+	y = left_height - right_height <= -2;
+
+	return ((x || y) ? left_height - right_height : left_height - right_height);
 }

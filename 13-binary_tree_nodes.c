49033d330edf8b0f2e1d944a5374c38dc8435beb
LwaziNcubeX <lwazincubex@gmail.com>
2023-11-01 22:00:41 +0200

    Updated binary_tree_nodes function to count nodes with at least 1 child.

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,5 +13,10 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 	}
 
-	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
+	if (tree->left != NULL || tree->right != NULL)
+	{
+		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
+	}
+
+	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 }
